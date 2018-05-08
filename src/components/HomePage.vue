6f4312cdaef9e21db92495dad0cf770ff1addfaf
Paul McCarthy <phantasos42@gmail.com>
2018-05-07 22:18:56 -0500

    renaming component

---
+++ src/components/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="x in test">{{x}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      msg: 'Component specific header',
+      test: [1, 2, 3, 4]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import HelloWorld from "@/components/HelloWorld";
-import test from "@/components/test";
+import HomePage from "@/components/HomePage";
 
 Vue.use(Router);
 
@@ -9,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "HelloWorld",
-      component: HelloWorld
+      name: "HomePage",
+      component: HomePage
     }
   ]
 });
