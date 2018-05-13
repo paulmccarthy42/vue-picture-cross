f32358fedad0bb4cd48f4107a42b27e594b1bf37
Paul McCarthy <phantasos42@gmail.com>
2018-05-13 17:32:20 -0500

    remove board specific methods from home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -27,42 +27,6 @@ export default {
       .catch(function (error) { console.log(error) })
   },
   methods: {
-    toggleDisplay: function (cell) {
-      this.cells.forEach(function (vueCell) {
-        if (vueCell === cell) {
-          vueCell.shownFilled = !vueCell.shownFilled
-        }
-      })
-    },
-    row: function (rowNumber) {
-      return this.cells.filter(function (cell) {
-        return cell.x_position === rowNumber
-      })
-    },
-    column: function (columnNumber) {
-      return this.cells.filter(function (cell) {
-        return cell.y_position === columnNumber
-      })
-    },
-    countCheck: function (groupOfCells, typeOfCheck) {
-      var count = [0]
-      groupOfCells.forEach(function (cell) {
-        if (cell[typeOfCheck]) {
-          count[count.length - 1] += 1
-        } else {
-          count.push(0)
-        }
-      })
-      var cleanCount = count.filter(function (num) { return num !== 0 })
-      if (cleanCount.length > 0) {
-        return cleanCount.join(' ')
-      } else {
-        return 0
-      }
-    },
-    sectionComplete: function (groupOfCells) {
-      return this.countCheck(groupOfCells, 'filled') === this.countCheck(groupOfCells, 'shownFilled')
-    }
   }
 }
 </script>
@@ -83,67 +47,6 @@ li {
 }
 a {
   color: #42b983;
-}
-
-.box {
-  border: 1px black solid;
-  height: 30px;
-  width: 30px;
-  float: left;
-}
-.row {
-  min-width: 330px;
-  height: 32px;
-}
-.puzzle {
-  padding-left: 33%;
-  padding-right: 33%;
-}
-.grid {
-  width: 450px;
-}
-.shaded.filled {
-  background-color: black;
-  color: white;
-}
-.shaded {
-  background-color: red;
-}
-.counts-left {
-  float: left;
-}
-.counts-top {
-  /*float: none;*/
-  /*padding-left: 100px;*/
-}
-.counts-top .cornerstone {
-  min-width: 105px;
-  float: left;
-}
-.count {
-  text-align: right;
-  color: grey;
-}
-.counts-left .count {
-  height: 30px;
-  border-top: 1px grey solid;
-  border-bottom: 1px grey solid;
-  min-width: 100px;
-  padding-right: 5px;
-}
-.counts-top .count {
-  width: 30px;
-  padding-bottom: 5px;
-  height: 50px;
-  float: left;
-  border-left: 1px grey solid;
-  border-right: 1px grey solid;
-}
-.count.completed {
-  background-color: lightgrey;
-}
-.top {
-  width: 100%;
 }
 
 </style>
